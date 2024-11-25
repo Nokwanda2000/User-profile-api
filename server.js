10d0483320daeadf47ad54939387705d2e59bbdb
Nokwanda2000 <nokwandachonco262@gmail.com>
2024-11-25 16:09:38 +0200

    added .env file

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require('express');
-const server= express();
-const PORT = 5050;
+const userRouter = require('./routes/userRoutes');
+const HeadersHandler = require('./routes/userRoutes');
+const app= express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+// app.use( path = '/user', userRouter);
+// app.use(HeadersHandler);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.get('/', (req, res) => {
+    res.render("welcome");
+});
+app.get('/signup', (req, res) => {
+    res.render("signup");
+});
+app.post('/users', (req, res) => {
+    res.send(`Successfully signed up`);
+});
+app.get('/users', (req, res) => {
+    res.render("users");})
 
 
 
@@ -10,8 +31,6 @@ const PORT = 5050;
 
 
 
-
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
